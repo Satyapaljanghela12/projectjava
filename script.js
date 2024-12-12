5e7b3a0fc56bd16ff7d508f6a3350c25bb7695c2
Satyapaljanghela12 <satyapaljanghela@gmail.com>
2024-12-12 22:12:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+var typed = new Typed(".autotype",{
+  strings : [
+    "FOR A BRIGHT FUTURE","TO INNOVATE SOMETHING BIG","TO ACHIEVE YOUR DREAM","TO FULFILL YOUR GOAL"
+  ],
+  typeSpeed: 50,
+  backSpeed:70,
+  loop:true,
+
+})
+
+
+
+
+
